6975e17e86e25074fedcb96e39d99f4f3ef6febd
sixmillions <liubw95@163.com>
2023-04-23 04:51:21 +0000

    feat: 修改脚本，从release下载memos

--- main.sh
+++ main.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if [ $# -eq 1 ]; then
+  version=$1
+else
+  version="$(curl -fsSLI -o /dev/null -w "%{url_effective}" https://github.com/sixmillions/memos-replit/releases/latest)"
+  version="${version#https://github.com/sixmillions/memos-replit/releases/tag/}"
+fi
+
+echo "down load memos ${version} >>> $(date '+%Y-%m-%d %H:%M:%S')"
+
+rm -rf memos && wget https://github.com/sixmillions/memos-replit/releases/download/${version}/memos
+
 echo "run memos >>> $(date '+%Y-%m-%d %H:%M:%S')"
 
 chmod +x ./memos
