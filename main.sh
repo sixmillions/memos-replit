6f4870f6b11ad2e35a5b9532a4abe1a4643d703c
sixmillions <41658874+sixmillions@users.noreply.github.com>
2023-05-26 09:43:51 +0800

    feat: 增加图片存放路径
    v0.13.0版本需要创建一个图片缓存路径，如果没有该路径，图片显示有问题

--- main.sh
+++ main.sh
@@ -2,10 +2,13 @@
 
 echo "down load memos ${version} >>> $(date '+%Y-%m-%d %H:%M:%S')"
 
+# 下载memos
 if [ ! -e memos ];then
     if [ $# -eq 1 ]; then
+      # 下载指定版本
       version=$1
     else
+      # 下载最新版
       version="$(curl -fsSLI -o /dev/null -w "%{url_effective}" https://github.com/sixmillions/memos-replit/releases/latest)"
       version="${version#https://github.com/sixmillions/memos-replit/releases/tag/}"
     fi
@@ -14,6 +17,7 @@ fi
 
 echo "run memos >>> $(date '+%Y-%m-%d %H:%M:%S')"
 
+# 可指定权限
 chmod +x ./memos
 
 # 数据库文件存放的地方
@@ -21,8 +25,14 @@ if [ ! -d ./db ];then
     mkdir ./db
 fi
 
+# 图片缓存位置
+if [ ! -d ./db/.thumbnail_cache ];then
+    mkdir ./db/.thumbnail_cache
+fi
+
 MEMOS_URL=https://${REPL_SLUG}.${REPL_OWNER}.repl.co
 
 echo "click url: $MEMOS_URL"
 
+# 运行memos
 ./memos --data ./db --mode prod --port 6969
